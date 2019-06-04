c100a9a1569301e66a14fe1bb95d369a659135f7
Aleksandrs Safronovs <safrix@gmail.com>
2019-06-04 13:26:06 +0200

    search works(for title only so far), routes separated, everything populated

--- api/models/composer.js
+++ api/models/composer.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
+const Music = require("./music.js");
+
 
 const composerSchema = new mongoose.Schema({
     first_name:  String,
     last_name:  String,
     wiki_link:  String,
-    works: Array,
-    arrangements: Array,
+    works: [{ type: mongoose.Schema.Types.ObjectId, ref: Music }],
+    arrangements: [{ type: mongoose.Schema.Types.ObjectId, ref: Music }],
     is_person: Boolean
 })
 
---
+++ api/routes/all.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const router = express.Router();
+
+const Music = require("../models/music.js");
+const Composer = require("../models/composer.js");
+
+router.get('/all_music', (req, res) => {
+    Music.find({}).
+        populate("composer").
+        populate("arrangement").
+        exec((err,result)=>{  
+            if (err) res.send("error fetching all music. err msg: "+err);
+            if (!err) {
+                res.json(result)
+            };
+        })
+})
+router.get('/all_composers', (req, res) => {
+    Composer.find({}).
+        populate("works").
+        populate("arrangements").
+        exec((err,result)=>{
+            if (err) res.send("error fetching composers. err msg: "+err);
+            if (!err) {
+                res.json(result)
+            };
+        })
+})
+
+module.exports = router;
