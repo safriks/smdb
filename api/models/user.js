4f2f8da07513b2ef17e3fe32b3a861f0b9b897bc
Aleksandrs Safronovs <safrix@gmail.com>
2019-06-06 13:03:37 +0200

    model for choirs, create_choir route works

--- api/models/user.js
+++ api/models/user.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 const Music = require("./music.js");
+const Choir = require("./choir.js")
+
 
 const userSchema = new mongoose.Schema({
     is_conductor: Boolean,
+    conducts: { type: mongoose.Schema.Types.ObjectId, ref: Choir },
+    sings_in: { type: mongoose.Schema.Types.ObjectId, ref: Choir },
     email: String,
     first_name: String,
     last_name: String,
     password: String,
     voice: String,
-    uploads: Array,
+    uploads: [{ type: mongoose.Schema.Types.ObjectId, ref: Music }],
+    favourites: [{ type: mongoose.Schema.Types.ObjectId, ref: Music }]
 
 });
 
