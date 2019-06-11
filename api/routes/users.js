83c267f9e0a0ae9247259d99f7759450a85b7f71
Aleksandrs Safronovs <safrix@gmail.com>
2019-06-11 17:24:27 +0200

    edit user untested route

--- api/routes/users.js
+++ api/routes/users.js
@@ -76,6 +76,27 @@ router.get("/user_info", (req, res) => {
   }
 });
 
+router.post("/edit_user", (req, res) => {
+  User.findOneAndUpdate(
+    {_id: req.session.user._id},
+    {
+      is_conductor: req.body.is_conductor,
+      conducts: req.body.conducts,
+      sings_in: req.body.sings_in,
+      email: req.body.email,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      password: req.body.password,
+      voice: req.body.voice
+  })
+  .then(result=>{
+    res.status(200).json({ message: "Profile updated" });
+  })
+  .catch(err=>{
+    res.status(500).json({ message: `not updated! ${err}` });
+  })
+})
+
 router.post("/log_out", (req, res) => {
   if (req.session.user) {
     req.session.destroy();
