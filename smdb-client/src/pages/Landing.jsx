f4de6036c31e26d2de4020f47fdc7740ff7f4be6
Steven Lindelauff <slindelauff@gmail.com>
2019-06-13 11:41:51 +0200

    added logo to nav, still has weird background though

--- smdb-client/src/pages/Landing.jsx
+++ smdb-client/src/pages/Landing.jsx
@@ -26,30 +26,34 @@ export default class Landing extends Component {
 
     return (
       <div className="columns is-centered">
-        <div className="column is-half container flex-column col">
+        <div className="column is-half container flex-column col no-border">
           <img className="logo" src={logo} alt="smdb-logo" />
-          <h1 className="header">Welcome to Oh Sheet Music Database!</h1>
-          {isUserLoggedIn ? (
-            <></>
-          ) : (
-            <>
-              <Link to="/log_in" className="button btn-landing btn-black">
-                Log in
-              </Link>
-              <Link to={"/sign_up"} className="button btn-landing btn-black">
-                Sign up
-              </Link>
-            </>
-          )}
-          <Link to={"/all_music"}>
+          <h1 className="header is-white">
+            Welcome to Oh Sheet Music Database!
+          </h1>
+          <div className="flex">
             {isUserLoggedIn ? (
-              <button className="button btn-landing btn-black">Browse</button>
+              <></>
             ) : (
-              <button className="button btn-landing btn-black">
-                Browse as guest
-              </button>
+              <>
+                <Link to="/log_in" className="button btn-landing btn-black">
+                  Log in
+                </Link>
+                <Link to={"/sign_up"} className="button btn-landing btn-black">
+                  Sign up
+                </Link>
+              </>
             )}
-          </Link>
+            <Link to={"/all_music"}>
+              {isUserLoggedIn ? (
+                <button className="button btn-landing btn-black">Browse</button>
+              ) : (
+                <button className="button btn-landing btn-black">
+                  Browse as guest
+                </button>
+              )}
+            </Link>
+          </div>
         </div>
       </div>
     );
