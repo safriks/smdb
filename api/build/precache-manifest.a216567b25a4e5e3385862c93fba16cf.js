22e799ccdd4ed468928c6fb2db32856c1526bbc1
Aleksandrs Safronovs <safrix@gmail.com>
2019-06-13 10:44:18 +0200

    rebuilt with proper api url

---
+++ api/build/precache-manifest.a216567b25a4e5e3385862c93fba16cf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4a10528a22486a4778f85920baaa4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2563b55518d6f13112db",
+    "url": "/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "68cdc24394f268ff8715",
+    "url": "/static/css/main.893625b6.chunk.css"
+  },
+  {
+    "revision": "2563b55518d6f13112db",
+    "url": "/static/js/2.86f76251.chunk.js"
+  },
+  {
+    "revision": "68cdc24394f268ff8715",
+    "url": "/static/js/main.452de7b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2d1b252709aae7e71b23e33834d14ae5",
+    "url": "/static/media/Alegreya-Regular.2d1b2527.otf"
+  },
+  {
+    "revision": "805307958e1320260dd732b7c41a2f15",
+    "url": "/static/media/black-and-white-blur-document-164821.80530795.jpg"
+  },
+  {
+    "revision": "0c72c7681cc97b79ca941e606fdb4a08",
+    "url": "/static/media/osmdb-logo.0c72c768.svg"
+  }
+]);
--- api/build/service-worker.js
+++ api/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e97fa669dca207cd607fcc3cde33ee6b.js"
+  "/precache-manifest.a216567b25a4e5e3385862c93fba16cf.js"
 );
 
 self.addEventListener('message', (event) => {
